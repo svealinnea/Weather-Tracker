54d4afc9e7070d797bf8fcc704541ae2da9bea37
Svea Wade <svealinneawade@gmail.com>
2020-11-16 11:20:55 -0800

    added the precipitaion feature
    Co-authored-by: David Sterry <sterry.david@gmail.com>
    Co-authored-by: Callie Stump <callie@stu.mp>

--- src/main.js
+++ src/main.js
@@ -25,7 +25,8 @@ $(document).ready(function() {
     function getElements(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature is ${parseInt((response.main.temp) - 273.15) * 9/5 + 32} °F.`);
-      $('.showFeels').text(`The temperature feels like ${parseInt((response.main.feels_like)- 273.15) * 9/5 + 32} °F`);
+      $('.showFeels').text(`The temperature feels like ${parseInt((response.main.feels_like) - 273.15) * 9/5 + 32} °F`);
+      $('.showPrecipitation').text(`There is currently ${response.weather[0].description} outside`)
     }
   });
 });
