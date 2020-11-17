67567b9693f23d0c10c83c11ba168c2cef2e2059
Svea Wade <svealinneawade@gmail.com>
2020-11-17 10:06:40 -0800

    Seperated promise logic
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,32 +1,32 @@
-
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import {} from './weather.js';
+import WeatherService from './weather.js';
+
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("")
+  $('.showHumidity').text("");
+  $('.showTemp').text("")
+  $('.showFeels').text("");
+  $('.showPrecipitation').text("");
+}
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
+    let city = $('#location').val();
+    clearFields();
+    let promise = WeatherService.getWeather(city);
+    promise.then(function(response) {
+      const body = JSON.parse(response)
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature is ${parseInt((response.main.temp) - 273.15) * 9/5 + 32} °F.`);
       $('.showFeels').text(`The temperature feels like ${parseInt((response.main.feels_like) - 273.15) * 9/5 + 32} °F`);
-      $('.showPrecipitation').text(`There is currently ${response.weather[0].description} outside`)
-    }
+      $('.showPrecipitation').text(`There is currently ${response.weather[0].description} outside`) 
+    },
+      function(error) {
+        $('.showErrors').text('There was an error processing your request: ${error}');
+      });
+    });
   });
-});
